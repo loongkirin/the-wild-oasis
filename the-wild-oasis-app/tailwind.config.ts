687ca50820fb6d35d739c48b2a1a46925f6c63e3
loongkirin <jack.yuan8088@gmail.com>
2024-11-14 23:57:28 +0800

    pre version 0.0.1

---
+++ the-wild-oasis-app/tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  corePlugins: {
+    preflight: false,
+  },
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  // important: '#root',
+  // important: '#__next',
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
